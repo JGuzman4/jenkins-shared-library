fb8a681102359422a585c9f2e1c1cecec21d338b
jguzman4 <jguzman23@mail.csuchico.edu>
2020-06-14 15:13:04 -0700

    fixing dir path

--- vars/packerBuild.groovy
+++ vars/packerBuild.groovy
@@ -1,5 +1,7 @@
 def call(Map params = [:]) {
   def templatePath = params.get('template_path', '.')
-  git branch: "master", url: 'git@github.com:JGuzman4/scripts.git'
+  dir('scripts') {
+    git branch: "master", url: 'git@github.com:JGuzman4/scripts.git'
+  }
   sh "./scripts/sh/packerBuild.sh ${templatePath}"
 }
