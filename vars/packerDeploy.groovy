f417ea02e96a8701f302ad9acc8f703d0913dce9
jguzman4 <jguzman23@mail.csuchico.edu>
2020-06-14 19:55:16 -0700

    fixing method

--- vars/packerDeploy.groovy
+++ vars/packerDeploy.groovy
@@ -1,13 +1,13 @@
 def call(Map params = [:]) {
 
-  def artifactList = params.get('artifacts', '')
-
-  def artifacts = artifactList.join(' ')
-
-
-
+  def artifacts = params.get('artifacts', '')
   dir('scripts') {
     git branch: "master", url: 'git@github.com:JGuzman4/scripts.git'
   }
-  sh "./scripts/sh/packerDeploy.sh ${artifacts}"
+
+  artifacts.each { artifact ->
+    sh "./scripts/sh/packerDeploy.sh ${artifact}"
+  }
+
+
 }
