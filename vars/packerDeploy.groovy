b01e03b6be76e0654bc5b4390edde6af5d6beb90
jguzman4 <jguzman23@mail.csuchico.edu>
2020-06-14 19:04:04 -0700

    adding method for packer deployment

---
+++ vars/packerDeploy.groovy
@@ -0,0 +1,13 @@
+def call(Map params = [:]) {
+
+  def artifactList = params.get('artifacts', '')
+
+  def artifacts = artifactList.join(',')
+
+
+
+  dir('scripts') {
+    git branch: "master", url: 'git@github.com:JGuzman4/scripts.git'
+  }
+  sh "./scripts/sh/packerDeploy.sh ${artifacts}"
+}
