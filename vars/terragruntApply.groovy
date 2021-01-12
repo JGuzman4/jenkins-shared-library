bd8fe8c0aba81121c624677630ea6d73aa92f274
jguzman4 <jguzman23@mail.csuchico.edu>
2021-01-11 22:27:10 -0800

    removing tfstate

--- vars/terragruntApply.groovy
+++ vars/terragruntApply.groovy
@@ -1,11 +1,10 @@
 def call(Map params = [:]) {
 
   def tfpath  = params.get('tfpath', '')
-  def tfstate = params.get('tfstate', '')
 
   dir('scripts') {
     git credentialsId: "git", branch: "master", url: 'git@github.com:JGuzman4/scripts.git'
   }
 
-  sh "./scripts/sh/terragruntApply.sh ${tfpath} ${tfstate}"
+  sh "./scripts/sh/terragruntApply.sh ${tfpath}"
 }
